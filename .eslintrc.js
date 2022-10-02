42635712e6fb07454a5d55f8438556d2b62b0f5c
xamfy <spratyaksh22@gmail.com>
2022-10-02 12:32:44 +0530

    ops: add jsx-runtime in eslint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  extends: ['plugin:react/jsx-runtime', 'plugin:react/recommended', 'standard-with-typescript', 'prettier'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
