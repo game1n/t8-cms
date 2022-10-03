8c13d7047834a944b1e708292e5155a074c6c462
egor.leontev <egor.leontev@sperasoft.com>
2022-10-03 16:50:18 +0200

    21 refactoring/router: refactor routing:
    - move out components in initialization from RouterConfig since they are static
    - refactor RouterConfig to integrate with refactored code

--- src/navigation/RouterConfig.tsx
+++ src/navigation/RouterConfig.tsx
@@ -1,35 +1,33 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
-import { useState, useEffect, ReactElement } from 'react';
+import React, { ReactElement } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
 import routerPaths from './index';
 import { NotFound } from './NotFound';
 import Home from '../screens/Home';
 import Login from '../screens/Login';
+
+import { getSupabaseData } from '../services/supabase.service';
+
+const NonAuthPages = (): ReactElement => (
+  <Routes>
+    <Route path="/" element={<Navigate to={routerPaths.Login} />} />
+    <Route path="/home" element={<Navigate to={routerPaths.Login} />} />
+    <Route path={routerPaths.Login} element={<Login />} />
+    <Route path={routerPaths.NoRouteFound} element={<NotFound />} />
+  </Routes>
+);
+
+const AuthPages = (): ReactElement => (
+  <Routes>
+    <Route path="/login" element={<Navigate to={routerPaths.Home} />} />
+    <Route path="/" element={<Navigate to={routerPaths.Home} />} />
+    <Route path={routerPaths.Home} element={<Home />} />
+  </Routes>
+);
+
 const RouterConfig = (): ReactElement => {
-  const [sessionPersist, setSessionPersist] = useState<any>(null);
-  useEffect(() => {
-    const sessionData: any = localStorage.getItem('persist-session');
-    setSessionPersist(JSON.parse(sessionData));
-  }, []);
-  const NonAuthPages = () => {
-    return (
-      <Routes>
-        <Route path="/" element={<Navigate to={routerPaths.Login} />} />
-        <Route path="/home" element={<Navigate to={routerPaths.Login} />} />
-        <Route path={routerPaths.Login} element={<Login />} />
-        <Route path={routerPaths.NoRouteFound} element={<NotFound />} />
-      </Routes>
-    );
-  };
-  const AuthPages = () => {
-    return (
-      <Routes>
-        <Route path="/login" element={<Navigate to={routerPaths.Home} />} />
-        <Route path="/" element={<Navigate to={routerPaths.Home} />} />
-        <Route path={routerPaths.Home} element={<Home />} />
-      </Routes>
-    );
-  };
-  return sessionPersist?.session ? <AuthPages /> : <NonAuthPages />;
+  const { session } = getSupabaseData();
+  return session ? <AuthPages /> : <NonAuthPages />;
 };
 
 export default RouterConfig;
