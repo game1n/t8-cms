3eb17d50a78a7b5e9ecd3e68d4f58b152507487b
egor.leontev <egor.leontev@sperasoft.com>
2022-10-04 13:45:24 +0200

    15 [common components]/loaders: replace regular loader to circular and linear based on the comment https://github.com/game1n/t8-cms/pull/19#issuecomment-1265899811

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,12 +1,37 @@
-import { CircularProgress, CircularProgressProps } from '@mui/material';
-import React, { ReactElement } from 'react';
+import React, { FunctionComponent, ReactElement } from 'react';
 
-export type LoaderProps = CircularProgressProps & {
+import {
+  CircularProgress,
+  CircularProgressProps,
+  LinearProgress,
+  LinearProgressProps,
+} from '@mui/material';
+
+export type LoaderProps = {
   shouldDisplayLoader: boolean;
 };
 
-export const Loader = ({
+export type CircularLoaderProps = LoaderProps & CircularProgressProps;
+export type LinearLoaderProps = LoaderProps & LinearProgressProps;
+
+export type MUILoadersProps = CircularLoaderProps | LinearProgressProps;
+
+function renderLoader<T extends MUILoadersProps>(
+  shouldRenderLoader: boolean,
+  Component: FunctionComponent<T>,
+  props: T
+): ReactElement | null {
+  return shouldRenderLoader ? <Component {...props} /> : null;
+}
+
+export const CircularLoader = ({
   shouldDisplayLoader,
   ...props
-}: LoaderProps): ReactElement | null =>
-  shouldDisplayLoader ? <CircularProgress {...props} /> : null;
+}: CircularLoaderProps): ReactElement | null =>
+  renderLoader(shouldDisplayLoader, CircularProgress, props);
+
+export const LinearLoader = ({
+  shouldDisplayLoader,
+  ...props
+}: LinearLoaderProps): ReactElement | null =>
+  renderLoader(shouldDisplayLoader, LinearProgress, props);
